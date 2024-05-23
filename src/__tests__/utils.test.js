b489939e04f71eb4e416172bf3e7f9a203f65068
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-05-23 19:35:50 +0300

    final

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,20 @@
-// Your tests here
+// src/__tests__/isPalindrome.test.js
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true for an empty string", () => {
+    expect(isPalindrome("")).toBe(true);
+  });
+
+  it("returns true for a single character", () => {
+    expect(isPalindrome("a")).toBe(true);
+  });
+
+  it("returns true for a simple palindrome", () => {
+    expect(isPalindrome("mom")).toBe(true);
+  });
+
+  it("returns false for a non-palindrome", () => {
+    expect(isPalindrome("hello")).toBe(false);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
-// Your code here
+// src/utils.js
+export function isPalindrome(str) {
+    const sanitizedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    return sanitizedStr === sanitizedStr.split("").reverse().join("");
+  }
+  
+//Pseudo code
+/* tolowercase-
+split==.split method
+reverse== .reverse method
+join==.join method*/
